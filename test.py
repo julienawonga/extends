f9f01eafbf7ce79014e3332ae4f1b49bdc9c5ec6
Julien Awonga <julienawon@gmail.com>
2023-08-17 13:35:06 +0100

    update pot name

--- test.py
+++ test.py
@@ -16,22 +16,24 @@ from tflite_runtime.interpreter import load_delegate
 
 class VideoStream:
     """Camera object that controls video streaming from the Picamera"""
-    def __init__(self,resolution=(640,480),framerate=30):
+
+    def __init__(self, resolution=(640, 480), framerate=30):
         # Initialize the PiCamera and the camera image stream
         self.stream = cv2.VideoCapture(0)
-        ret = self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
-        ret = self.stream.set(3,resolution[0])
-        ret = self.stream.set(4,resolution[1])
-            
+        ret = self.stream.set(cv2.CAP_PROP_FOURCC,
+                              cv2.VideoWriter_fourcc(*'MJPG'))
+        ret = self.stream.set(3, resolution[0])
+        ret = self.stream.set(4, resolution[1])
+
         # Read first frame from the stream
         (self.grabbed, self.frame) = self.stream.read()
 
-	# Variable to control when the camera is stopped
+        # Variable to control when the camera is stopped
         self.stopped = False
 
     def start(self):
-	# Start the thread that reads frames from the video stream
-        Thread(target=self.update,args=()).start()
+        # Start the thread that reads frames from the video stream
+        Thread(target=self.update, args=()).start()
         return self
 
     def update(self):
@@ -47,18 +49,17 @@ class VideoStream:
             (self.grabbed, self.frame) = self.stream.read()
 
     def read(self):
-	# Return the most recent frame
+        # Return the most recent frame
         return self.frame
 
     def stop(self):
-	# Indicate that the camera and thread should be stopped
+        # Indicate that the camera and thread should be stopped
         self.stopped = True
-
 
 
 MODEL_NAME = "modeldir"
 GRAPH_NAME = "detect_quant.tflite"
-LABELMAP_NAME = "pot_labels_map.txt"
+LABELMAP_NAME = "pot_label_map.txt"
 min_conf_threshold = float(0.9)
 resW, resH = 1280, 720
 imW, imH = int(resW), int(resH)
@@ -68,10 +69,10 @@ imW, imH = int(resW), int(resH)
 CWD_PATH = os.getcwd()
 
 # Path to .tflite file, which contains the model that is used for object detection
-PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,GRAPH_NAME)
+PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME, GRAPH_NAME)
 
 # Path to label map file
-PATH_TO_LABELS = os.path.join(CWD_PATH,MODEL_NAME,LABELMAP_NAME)
+PATH_TO_LABELS = os.path.join(CWD_PATH, MODEL_NAME, LABELMAP_NAME)
 
 # Load the label map
 with open(PATH_TO_LABELS, 'r') as f:
@@ -81,7 +82,7 @@ with open(PATH_TO_LABELS, 'r') as f:
 # https://www.tensorflow.org/lite/models/object_detection/overview
 # First label is '???', which has to be removed.
 if labels[0] == '???':
-    del(labels[0])
+    del (labels[0])
 
 # Load the Tensorflow Lite model.
 interpreter = Interpreter(model_path=PATH_TO_CKPT)
@@ -103,9 +104,9 @@ input_std = 127.5
 # because outputs are ordered differently for TF2 and TF1 models
 outname = output_details[0]['name']
 
-if ('StatefulPartitionedCall' in outname): # This is a TF2 model
+if ('StatefulPartitionedCall' in outname):  # This is a TF2 model
     boxes_idx, classes_idx, scores_idx = 1, 3, 0
-else: # This is a TF1 model
+else:  # This is a TF1 model
     boxes_idx, classes_idx, scores_idx = 0, 1, 2
 
 # Initialize frame rate calculation
@@ -113,10 +114,10 @@ frame_rate_calc = 1
 freq = cv2.getTickFrequency()
 
 # Initialize video stream
-videostream = VideoStream(resolution=(imW,imH),framerate=30).start()
+videostream = VideoStream(resolution=(imW, imH), framerate=30).start()
 time.sleep(1)
 
-#for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
+# for frame1 in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
 while True:
 
     # Start timer (for calculating frame rate)
@@ -136,13 +137,16 @@ while True:
         input_data = (np.float32(input_data) - input_mean) / input_std
 
     # Perform the actual detection by running the model with the image as input
-    interpreter.set_tensor(input_details[0]['index'],input_data)
+    interpreter.set_tensor(input_details[0]['index'], input_data)
     interpreter.invoke()
 
     # Retrieve detection results
-    boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[0] # Bounding box coordinates of detected objects
-    classes = interpreter.get_tensor(output_details[classes_idx]['index'])[0] # Class index of detected objects
-    scores = interpreter.get_tensor(output_details[scores_idx]['index'])[0] # Confidence of detected objects
+    boxes = interpreter.get_tensor(output_details[boxes_idx]['index'])[
+        0]  # Bounding box coordinates of detected objects
+    classes = interpreter.get_tensor(output_details[classes_idx]['index'])[
+        0]  # Class index of detected objects
+    scores = interpreter.get_tensor(output_details[scores_idx]['index'])[
+        0]  # Confidence of detected objects
 
     # Loop over all detections and draw detection box if confidence is above minimum threshold
     for i in range(len(scores)):
@@ -150,23 +154,31 @@ while True:
 
             # Get bounding box coordinates and draw box
             # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
-            ymin = int(max(1,(boxes[i][0] * imH)))
-            xmin = int(max(1,(boxes[i][1] * imW)))
-            ymax = int(min(imH,(boxes[i][2] * imH)))
-            xmax = int(min(imW,(boxes[i][3] * imW)))
-            
-            cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
+            ymin = int(max(1, (boxes[i][0] * imH)))
+            xmin = int(max(1, (boxes[i][1] * imW)))
+            ymax = int(min(imH, (boxes[i][2] * imH)))
+            xmax = int(min(imW, (boxes[i][3] * imW)))
+
+            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (10, 255, 0), 2)
 
             # Draw label
-            object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
-            label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
-            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
-            label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
-            cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
-            cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
+            # Look up object name from "labels" array using class index
+            object_name = labels[int(classes[i])]
+            label = '%s: %d%%' % (object_name, int(
+                scores[i]*100))  # Example: 'person: 72%'
+            labelSize, baseLine = cv2.getTextSize(
+                label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)  # Get font size
+            # Make sure not to draw label too close to top of window
+            label_ymin = max(ymin, labelSize[1] + 10)
+            # Draw white box to put label text in
+            cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (
+                xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED)
+            cv2.putText(frame, label, (xmin, label_ymin-7),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)  # Draw label text
 
     # Draw framerate in corner of frame
-    cv2.putText(frame,'FPS: {0:.2f}'.format(frame_rate_calc),(30,50),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2,cv2.LINE_AA)
+    cv2.putText(frame, 'FPS: {0:.2f}'.format(frame_rate_calc), (30, 50),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
 
     # All the results have been drawn on the frame, so it's time to display it.
     cv2.imshow('Object detector', frame)
@@ -174,7 +186,7 @@ while True:
     # Calculate framerate
     t2 = cv2.getTickCount()
     time1 = (t2-t1)/freq
-    frame_rate_calc= 1/time1
+    frame_rate_calc = 1/time1
 
     # Press 'q' to quit
     if cv2.waitKey(1) == ord('q'):
